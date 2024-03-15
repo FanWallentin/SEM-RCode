75b62b07397effb8ef26f91aa2a0edf3e4c1c132
nyj933 <nyj933@gmail.com>
2024-03-15 16:53:06 +0100

    updates

--- Examples-note1-Rcode/PredictG1&G2.R
+++ Examples-note1-Rcode/PredictG1&G2.R
@@ -1,34 +1,60 @@
 ##Prediction of Grade Averages
-library(lavaan)
+require(lavaan)
 
 urlfile="https://raw.github.com/FanWallentin/SEM-RCode/main/Examples-note1-Rcode/Predict.DAT"
 PredictG <- read.table(urlfile, header=F, sep= " ")
 
 colnames(PredictG) = c("GRAVEREQ", "GRAVELEC","KNOWLEDG", "IQPREVYR", "EDMOTIV")
-cov(PredictG)
+sapply(PredictG,class)
 
+Cov = cov(PredictG)
+names(Cov) = c("GRAVEREQ", "GRAVELEC","KNOWLEDG", "IQPREVYR", "EDMOTIV")
+
+
+## Error terms are not correlated
 model1 <- '
        
          GRAVEREQ ~ KNOWLEDG + IQPREVYR + EDMOTIV
          GRAVELEC ~ KNOWLEDG + IQPREVYR + EDMOTIV
-         
          GRAVEREQ ~~ 0*GRAVELEC
-         
 '
-fit1 = lavaan::cfa(model1, data= PredictG,likelihood = "wishart",
-                   std.lv=T,fixed.x=F)
+fit1 = lavaan::sem(model1, sample.cov = Cov, sample.nobs = 2000 ,likelihood = "wishart",
+                   fixed.x=F,std.lv = T)
 summary(fit1, standardized=TRUE,rsquare=T,fit.measures = TRUE)
+fitMeasures(fit1) ## AIC = 2t - 2ln(L)
 
+
+## Path diagram
+require(semPlot)
+m <- matrix(c(NA,"KNOWLEDG",     NA,  "GRAVEREQ",
+              NA,NA,  NA,   NA,
+              "IQPREVYR",NA,  NA, NA,
+              NA, NA,    NA,"GRAVELEC",
+              NA,"EDMOTIV",   NA,  NA), byrow = TRUE, 5, 4)
+
+
+semPaths(fit1, whatLabels = "est",
+         sizeMan = 10, edge.label.cex = 0.75,
+         style = "ram",
+         nCharNodes = 0, nCharEdges = 0,
+        layout = m)
+
+
+## Error term are correlated
 
 model2 <- '
        
          GRAVEREQ ~ KNOWLEDG + IQPREVYR + EDMOTIV
          GRAVELEC ~ KNOWLEDG + IQPREVYR + EDMOTIV
-         
          GRAVEREQ ~~ GRAVELEC
 '
-fit2 = lavaan::sem(model2,data = PredictG,std.lv=T,likelihood = "wishart")
+fit2 = lavaan::cfa(model2,data = PredictG, likelihood = "wishart",
+                   std.lv=T,fixed.x=F)
+#fitMeasures(fit2) 
 summary(fit2, standardized=TRUE,rsquare=T,fit.measures = TRUE)
-install.packages("usethis")
-E
 
+semPaths(fit2, whatLabels = "est",
+         sizeMan = 10, edge.label.cex = 0.75,
+         style = "ram",
+         nCharNodes = 0, nCharEdges = 0,
+         layout = m)
