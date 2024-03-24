19236b123d4eee261afd34d4d71344783b9ce065
nyj933 <nyj933@gmail.com>
2024-03-24 21:22:22 +0100

    updates

--- Examples-note1-Rcode/Split_groups.R
+++ Examples-note1-Rcode/Split_groups.R
@@ -16,12 +16,9 @@ GW = as.data.frame(group_split(school)[1][[1]])
 PA = as.data.frame(group_split(school)[2][[1]])
 
 require(MVN)
-mvn(hsschools[,5:13],univariateTest = "SW",multivariatePlot = "qq")
-mtext(paste0("(", "HSSCHOOL", ")"), side=3)
-mvn(GW[,5:13],univariateTest = "SW",multivariatePlot = "qq")
-mtext(paste0("(", "GW", ")"), side=3)
-mvn(PA[,5:13],univariateTest = "SW",multivariatePlot = "qq")
-mtext(paste0("(", "PA", ")"), side=3)
+mvn(hsschools[,5:13],univariateTest = "SW",multivariatePlot = "qq"); mtext(paste0("(", "HSSCHOOL", ")"), side=3)
+mvn(GW[,5:13],univariateTest = "SW",multivariatePlot = "qq"); mtext(paste0("(", "GW", ")"), side=3)
+mvn(PA[,5:13],univariateTest = "SW",multivariatePlot = "qq"); mtext(paste0("(", "PA", ")"), side=3)
 
 
            
