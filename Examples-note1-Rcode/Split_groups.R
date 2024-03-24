4da8626b62e70646b332df9b264a1522aafefd58
nyj933 <nyj933@gmail.com>
2024-03-24 22:25:39 +0100

    updates

--- Examples-note1-Rcode/Split_groups.R
+++ Examples-note1-Rcode/Split_groups.R
@@ -3,6 +3,7 @@
 hsschools <- read.delim("https://raw.github.com/FanWallentin/SEM-RCode/main/Examples-note1-Rcode/hsschools.txt")
 hsschools = hsschools[,c(1:13)]
 sapply(hsschools, class)
+hsschools[,1:4] <- sapply(hsschools[,1:4],as.factor)
 
 #install.packages("dplyr")
 require(dplyr)
@@ -10,7 +11,7 @@ require(dplyr)
 school = hsschools %>% 
   group_by(SCHOOL)
 
-prop.table(table(hsschools$SCHOOL))
+prop.table(table(hsschools[,1]))
 
 GW = as.data.frame(group_split(school)[1][[1]])
 PA = as.data.frame(group_split(school)[2][[1]])
@@ -19,6 +20,74 @@ require(MVN)
 mvn(hsschools[,5:13],univariateTest = "SW",multivariatePlot = "qq"); mtext(paste0("(", "HSSCHOOL", ")"), side=3)
 mvn(GW[,5:13],univariateTest = "SW",multivariatePlot = "qq"); mtext(paste0("(", "GW", ")"), side=3)
 mvn(PA[,5:13],univariateTest = "SW",multivariatePlot = "qq"); mtext(paste0("(", "PA", ")"), side=3)
+
+
+
+#install.packages("ggplot2")
+library(ggplot2)
+library(ggpubr)
+theme_set(
+  theme_bw() +
+    theme(legend.position = "top")
+)
+
+## HSSCHOOLS
+a =  ggplot(hsschools[,1:4], aes(x = SCHOOL, fill = SCHOOL)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")+
+  scale_fill_manual(labels=c("GW","PA"),values=c('tomato','cyan3'))
+b = ggplot(hsschools[,1:4], aes(x = GENDER, fill = GENDER)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")+
+  scale_fill_manual(labels=c("female","male"),values=c('tomato','cyan3'))
+
+c =  ggplot(hsschools[,1:4], aes(x = AGEYEAR, fill = AGEYEAR)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")#+
+d =  ggplot(hsschools[,1:4], aes(x = BIRTHMON, fill = BIRTHMON)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")#+
+
+
+ggarrange(a,b,c,d,
+          ncol = 2, nrow = 2,labels = "HSSCHOOL" )
+
+## GW
+
+b_1 = ggplot(GW[,1:4], aes(x = GENDER, fill = GENDER)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")+
+  scale_fill_manual(labels=c("female","male"),values=c('tomato','cyan3'))
+
+c_1 =  ggplot(GW[,1:4], aes(x = AGEYEAR, fill = AGEYEAR)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")#+
+d_1 =  ggplot(GW[,1:4], aes(x = BIRTHMON, fill = BIRTHMON)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")#+
+
+
+ggarrange(ggarrange(b_1,c_1,
+          ncol = 2, nrow = 1 ),d_1,nrow = 2,labels = "GW")
+
+## PA
+b_2 = ggplot(PA[,1:4], aes(x = GENDER, fill = GENDER)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")+
+  scale_fill_manual(labels=c("female","male"),values=c('tomato','cyan3'))
+
+c_2 =  ggplot(PA[,1:4], aes(x = AGEYEAR, fill = AGEYEAR)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")#+
+d_2 =  ggplot(PA[,1:4], aes(x = BIRTHMON, fill = BIRTHMON)) +
+  geom_bar() +
+  geom_text(aes(label = ..count..), stat = "count", vjust = 1.5, colour = "white")#+
+
+
+ggarrange(ggarrange(b_2,c_2,
+                    ncol = 2, nrow = 1 ),d_2,nrow = 2,labels = "PA")
+
+
 
 
            
