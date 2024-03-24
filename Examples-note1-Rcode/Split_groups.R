c55ebf715fdfde049e8676c11c03185bf8a9d26b
nyj933 <nyj933@gmail.com>
2024-03-24 20:33:26 +0100

    updates

--- Examples-note1-Rcode/Split_groups.R
+++ Examples-note1-Rcode/Split_groups.R
@@ -1,15 +1,32 @@
 
 
-hsschools <- read.delim("C:/SEM-UU/2024/Examples/Lavaan/hsschools.txt")
+hsschools <- read.delim("https://raw.github.com/FanWallentin/SEM-RCode/main/Examples-note1-Rcode/hsschools.txt")
 hsschools = hsschools[,c(1:13)]
 sapply(hsschools, class)
 
 #install.packages("dplyr")
 require(dplyr)
-hsschools$SCHOOL = as.factor(hsschools$SCHOOL)
 
 school = hsschools %>% 
   group_by(SCHOOL)
 
-GW = group_split(school)[2]
-PA = group_split(school)[1]
+prop.table(table(hsschools$SCHOOL))
+
+GW = as.data.frame(group_split(school)[2][[2]])
+PA = as.data.frame(group_split(school)[2][[1]])
+
+require(MVN)
+mvn(GW[,3:13],univariateTest = "SW",multivariatePlot = "qq")
+mvn(PA[,3:13],univariateTest = "SW",multivariatePlot = "qq")
+
+## or
+summary(hsschools)
+
+## or
+library(psych)
+describeBy(hsschools[,3:13],
+           group = hsschools$SCHOOL,
+           digits= 4)
+
+
+           
